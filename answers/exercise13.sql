e445ff0233d07b875fad5d99f43dcaafe1ddc95a
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-12-01 19:43:34 -0500

    still same table, command is correct.

---
+++ answers/exercise13.sql
@@ -0,0 +1,9 @@
+mysql> SELECT * FROM students ORDER BY Country DESC, City DESC;
++-------------+-------------+---------+------------+----------+
+| StudentName | Address     | City    | Postalcode | Country  |
++-------------+-------------+---------+------------+----------+
+| Jane Doe    | 57 Union St | Glasgow | G13RB      | Scotland |
++-------------+-------------+---------+------------+----------+
+1 row in set (0.00 sec)
+
+mysql> 
