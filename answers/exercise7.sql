8fdc59e227df48f733a7cd222000a8845aaddd00
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-12-01 19:14:26 -0500

    Update exercise7.sql

---
+++ answers/exercise7.sql
@@ -0,0 +1,17 @@
+mysql> ALTER TABLE users DROP COLUMN Birthday;
+Query OK, 0 rows affected (0.03 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> describe users;
++-----------+--------------+------+-----+---------+-------+
+| Field     | Type         | Null | Key | Default | Extra |
++-----------+--------------+------+-----+---------+-------+
+| UserID    | int          | YES  |     | NULL    |       |
+| LastName  | varchar(255) | YES  |     | NULL    |       |
+| FirstName | varchar(255) | YES  |     | NULL    |       |
+| Address   | varchar(255) | YES  |     | NULL    |       |
+| City      | varchar(255) | YES  |     | NULL    |       |
++-----------+--------------+------+-----+---------+-------+
+5 rows in set (0.01 sec)
+
+mysql> 
