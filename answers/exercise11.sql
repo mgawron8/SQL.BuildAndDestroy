2a9a5b8b5381b12138afc949619ceecd6f85d32a
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-12-01 19:39:47 -0500

    not much of a list, but i know it goes in reverse

---
+++ answers/exercise11.sql
@@ -0,0 +1,9 @@
+ysql> SELECT * FROM students ORDER BY city;
++-------------+-------------+---------+------------+----------+
+| StudentName | Address     | City    | Postalcode | Country  |
++-------------+-------------+---------+------------+----------+
+| Jane Doe    | 57 Union St | Glasgow | G13RB      | Scotland |
++-------------+-------------+---------+------------+----------+
+1 row in set (0.00 sec)
+
+mysql> 
