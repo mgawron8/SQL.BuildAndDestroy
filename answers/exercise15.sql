84c5360c369a4d33d1b6d82ad468bf4dccb05628
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-12-01 19:47:33 -0500

    Update exercise15.sql

---
+++ answers/exercise15.sql
@@ -0,0 +1,9 @@
+mysql> SELECT * FROM Students WHERE PostalCode IS NOT NULL;
++-------------+-------------+---------+------------+----------+
+| StudentName | Address     | City    | Postalcode | Country  |
++-------------+-------------+---------+------------+----------+
+| Jane Doe    | 57 Union St | Glasgow | G13RB      | Scotland |
++-------------+-------------+---------+------------+----------+
+1 row in set (0.00 sec)
+
+mysql> 
